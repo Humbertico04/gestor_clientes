86c425fc5d96cc58d09f5c04bdf5ee6b27ace2bb
Humbertico04 <humbertico2004@gmail.com>
2023-02-14 13:48:18 +0100

    copilot

--- database.py
+++ database.py
@@ -1,57 +1,63 @@
+from config import DATABASE_PATH
 import csv
-import config
 
 class Cliente:
+    # Constructor con 3 parámetros
     def __init__(self, dni, nombre, apellido):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
-
+    # Función para imprimir
     def __str__(self):
             return f"({self.dni}) {self.nombre} {self.apellido}"
 
 class Clientes:
     # Creamos la lista y cargamos los clientes en memoria
     lista = []
-    with open(config.DATABASE_PATH, newline='\n') as fichero:
+    with open(DATABASE_PATH, newline='\n') as fichero:
         reader = csv.reader(fichero, delimiter=';')
         for dni, nombre, apellido in reader:
             cliente = Cliente(dni, nombre, apellido)
             lista.append(cliente)
 
+    # Busca un cliente por su DNI
     @staticmethod
     def buscar(dni):
         for cliente in Clientes.lista:
             if cliente.dni == dni:
                 return cliente
 
+    # Crea un nuevo cliente
     @staticmethod
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
-        Clientes.guardar() # new
+        Clientes.guardar()
         return cliente
 
+    # Modifica la información de un cliente
     @staticmethod
     def modificar(dni, nombre, apellido):
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 Clientes.lista[i].nombre = nombre
                 Clientes.lista[i].apellido = apellido
-                Clientes.guardar() # new
+                Clientes.guardar()
                 return Clientes.lista[i]
     
+    # Borra un cliente
     @staticmethod
     def borrar(dni):
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 cliente = Clientes.lista.pop(i)
-                Clientes.guardar() # new
+                Clientes.guardar()
                 return cliente
             
+    # Guarda los clientes en el fichero de la BD
     @staticmethod
     def guardar():
-        with open(config.DATABASE_PATH, "w", newline="\n") as fichero:
+        with open(DATABASE_PATH, "w", newline="\n") as fichero:
             writer = csv.writer(fichero, delimiter=";")
             for c in Clientes.lista:
                 writer.writerow((c.dni, c.nombre, c.apellido))
--- menu.py
+++ menu.py
@@ -19,43 +19,48 @@ def iniciar():
 
         opcion = input("> ")
         helpers.limpiar_pantalla()
-        if opcion == '1':
+        if opcion == '1': # Si el usuario ingresa 1, se listan los clientes
             print("Listando los clientes...\n")
-            for cliente in db.Clientes.lista:
-                print(cliente)
+            for cliente in db.Clientes.lista: # Se recorre la lista de clientes
+                print(cliente) # Se imprime cada cliente
 
-        if opcion == '2':
+        if opcion == '2': # Si la opción es 2, buscar un cliente
             print("Buscando un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
-            cliente = db.Clientes.buscar(dni)
-            print(cliente) if cliente else print("Cliente no encontrado.")
+            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper() # Lee el DNI del cliente
+            cliente = db.Clientes.buscar(dni) # Busca el cliente con el DNI introducido
+            print(cliente) if cliente else print("Cliente no encontrado.") # Si el cliente existe, se imprime, si no, se imprime que no existe
 
-        if opcion == '3':
+        if opcion == '3':  # Opción 3: Añadir un cliente
             print("Añadiendo un cliente...\n")
             # Comprobación de DNI válido
-            while 1:
-                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
-                if helpers.dni_valido(dni, db.Clientes.lista):
-                    break
-            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
-            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
-            db.Clientes.crear(dni, nombre, apellido)
+            while 1:  # Bucle infinito
+                dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()  # Pedir el DNI
+                if helpers.dni_valido(dni, db.Clientes.lista):  # Si el DNI es válido
+                    break  # Salir del bucle
+            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()  # Pedir el nombre
+            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()  # Pedir el apellido
+            db.Clientes.crear(dni, nombre, apellido)  # Crear el cliente
  
         if opcion == '4':
+            # Se muestra un mensaje de "Modificando un cliente" y se pide por teclado el DNI del cliente a modificar.
             print("Modificando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+            # Se busca el cliente por el DNI que se ha introducido por teclado.
             cliente = db.Clientes.buscar(dni)
             if cliente:
+                # Si se ha encontrado el cliente, se le pide por teclado el nombre y el apellido, que se guardan en la variable nombre y apellido.
                 nombre = helpers.leer_texto(2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
                 apellido = helpers.leer_texto(2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
+                # Se llama al método modificar del módulo Clientes y se le pasan los parámetros dni, nombre y apellido.
                 db.Clientes.modificar(cliente.dni, nombre, apellido)
                 print("Cliente modificado correctamente.")
             else:
+                # Si no se ha encontrado el cliente, se muestra un mensaje de "Cliente no encontrado."
                 print("Cliente no encontrado.")
 
-        if opcion == '5':
+        if opcion == '5': # Selecciona la opción 5 para borrar un cliente
             print("Borrando un cliente...\n")
-            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
+            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper() # Lee el DNI del cliente a borrar
             print("Cliente borrado correctamente.") if db.Clientes.borrar(dni) else print("Cliente no encontrado.")
 
         if opcion == '6':
