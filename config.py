4a498d39726abfa32e4df161163c5d0c5f798b5a
Humbertico04 <humbertico2004@gmail.com>
2023-02-17 00:19:33 +0100

    coments

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import sys
 
-DATABASE_PATH = 'clientes.csv'
+DATABASE_PATH = 'clientes.csv'  # Define a constant for the database path
 
 if 'pytest' in sys.argv[0]:
-    DATABASE_PATH = 'tests/clientes_tests.csv'
+    DATABASE_PATH = 'tests/clientes_tests.csv'  # If running tests, use the tests database
--- helpers.py
+++ helpers.py
@@ -1,21 +1,36 @@
+# Importar librerías de sistema operativo y expresiones regulares
 import os
 import platform
 import re
 
+# Definir función para limpiar la pantalla
 def limpiar_pantalla():
-    os.system('cls') if platform.system() == "Windows" else os.system('clear')
+    # Si el sistema operativo es Windows, usar cls
+    if platform.system() == "Windows":
+        os.system('cls')
+    # Si el sistema operativo es Linux o Mac, usar clear
+    else:
+        os.system('clear')
 
+# Definir función para leer texto
 def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
-    print(mensaje) if mensaje else None
+    # Mostrar mensaje si se pasa como argumento
+    if mensaje:
+        print(mensaje)
+    # Bucle para leer texto
     while True:
         texto = input("> ")
+        # Comprobar longitud
         if len(texto) >= longitud_min and len(texto) <= longitud_max:
             return texto
         
+# Definir función para validar un DNI
 def dni_valido(dni, lista):
+    # Comprobar si el DNI tiene el formato correcto
     if not re.match('[0-9]{2}[A-Z]$', dni):
         print("DNI incorrecto, debe cumplir el formato.")
         return False
+    # Comprobar si el DNI ya está en la lista de clientes
     for cliente in lista:
         if cliente.dni == dni:
             print("DNI utilizado por otro cliente.")
